ca2180af30da9f6c5815267ed8f8539a594f491a
Robert Weilbaecher <robertgweilbaecher@gmail.com>
2016-07-09 16:23:43 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+module.exports = function(io){
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+io.on('connection',function(socket){
+  console.log('a user connected');
+  socket.on('chatMessage', function(msg){
+    io.emit('chatMessage', msg);
+  });
+});
+
+return router;
+}
