ffacef0cbbaf01e16ec590626f9f70f3fe394b63
Robert Weilbaecher <robertgweilbaecher@gmail.com>
2016-07-09 22:10:48 -0500

    identified chat members

--- routes/index.js
+++ routes/index.js
@@ -7,10 +7,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-var numClients = 0
+var numClients = 0;
+var clientId = 0;
 io.on('connection',function(socket){
   numClients++;
-  io.emit('stats', { numClients: numClients, message: 'A new user has connected.' });
+  clientId++
+  var message = "You have connected and your id is " + clientId;
+  socket.emit('conn',{message:message,clientId:clientId});
+  var message = 'A new client has connected';
+  io.emit('stats', { numClients: numClients, message: message});
     console.log('Connected clients:', numClients);
   console.log('a user connected');
   socket.on('disconnect', function() {
@@ -26,8 +31,8 @@ io.on('connection',function(socket){
     io.emit('chatMessage', msg);
   });
   socket.on('event', function(data) {
-        console.log('A client sent us this dumb message:', data.message);
-        io.emit('Incoming', {message: data.message});
+        console.log('Client #',data.clientId,'sent us this dumb message:', data.message);
+        io.emit('Incoming', {message: data.message, clientId: data.clientId});
   });
 
 });
