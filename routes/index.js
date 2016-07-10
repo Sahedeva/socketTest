f6a5da00bd61564c081203064a2f65f4f9d2d391
Robert Weilbaecher <robertgweilbaecher@gmail.com>
2016-07-09 21:14:06 -0500

    got socket working

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,27 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var numClients = 0
 io.on('connection',function(socket){
+  numClients++;
+  io.emit('stats', { numClients: numClients, message: 'A new user has connected.' });
+    console.log('Connected clients:', numClients);
   console.log('a user connected');
-  socket.emit('announcements', {message: 'A new user has connected.'});
-  // socket.on('chatMessage', function(msg){
-  //   console.log(msg);
-  //   io.emit('chatMessage', msg);
-  // });
+  socket.on('disconnect', function() {
+        numClients--;
+        io.emit('stats', { numClients: numClients, message: 'A user has disconnected.'});
+
+        console.log('Connected clients:', numClients);
+    });
+
+  // socket.emit('announcements', {message: 'A new user has connected.'});
+  socket.on('chatMessage', function(msg){
+    console.log(msg);
+    io.emit('chatMessage', msg);
+  });
   socket.on('event', function(data) {
         console.log('A client sent us this dumb message:', data.message);
-        socket.emit('Incoming', {message: data.message});
+        io.emit('Incoming', {message: data.message});
   });
 
 });
